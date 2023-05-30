30b495b50eeb287e21761a44ae9ceffb4ab99416
Dat Tran <dat.tran@ghielectronics.com>
2023-05-30 08:29:34 -0400

    to 1.0.3

--- src/App.vue
+++ src/App.vue
@@ -151,15 +151,15 @@ export default {
                     isGlb: false,
                     image: null,
                 },
-				'Due_SC13_102': {
-                    title: 'Due_SC13 (v1.0.2)',
-                    url: 'firmware/due_sc13_v102.ghi',
+				'Due_SC13_103': {
+                    title: 'Due_SC13 (v1.0.3)',
+                    url: 'firmware/due_sc13_v103.ghi',
                     isGlb: false,
                     image: null,
                 },
-				'Due_SC007_102': {
-                    title: 'Due_SC007 (v1.0.2)',
-                    url: 'firmware/due_sc007_v102.ghi',
+				'Due_SC007_103': {
+                    title: 'Due_SC007 (v1.0.3)',
+                    url: 'firmware/due_sc007_v103.ghi',
                     isGlb: false,
                     image: null,
                 },
